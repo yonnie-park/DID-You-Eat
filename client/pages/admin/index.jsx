53ed4b85354a8e381d7d3d8cf0a2bfb96b08a8f8
oreozzang <93peace@naver.com>
2023-02-07 18:08:05 +0900

    add: Admin

---
+++ client/pages/admin/index.jsx
@@ -0,0 +1,13 @@
+import AdminHeader from "@/src/components/AdminHeader";
+import AdminLayout from "@/src/components/AdminLayout";
+import React from "react";
+
+const Admin = () => {
+  return (
+    <AdminLayout>
+      <div>hi admin</div>
+    </AdminLayout>
+  );
+};
+
+export default Admin;
