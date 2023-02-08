a15305b419e4e284fd3b75109b0a9aa0fcc14eb2
oreozzang <93peace@naver.com>
2023-02-08 13:45:44 +0900

    add: admin footer header

--- client/src/components/AdminHeader.jsx
+++ client/src/components/AdminHeader.jsx
@@ -1,3 +1,14 @@
+import Link from "next/link";
+
 export default function AdminHeader() {
-  return <div>헤더</div>;
+  return (
+    <nav className="admin-header">
+      <div className="admin-header__container">
+        <Link href="/admin">
+          <div className="admin-header__home-button">Did You Eat?</div>
+        </Link>
+        <div className="admin-header__connect-button">Connect Wallet</div>
+      </div>
+    </nav>
+  );
 }
--- client/src/components/AdminLayout.jsx
+++ client/src/components/AdminLayout.jsx
@@ -1,10 +1,21 @@
+import AdminFooter from "./AdminFooter";
 import AdminHeader from "./AdminHeader";
 
 export default function AdminLayout({ children }) {
   return (
     <div className="admin-layout">
       <AdminHeader></AdminHeader>
-      <div>{children}</div>
+      <div className="admin-main">
+        <div className="admin-main__container">{children}</div>
+      </div>
+      <AdminFooter></AdminFooter>
+      <style jsx global>
+        {`
+          body {
+            background: black;
+          }
+        `}
+      </style>
     </div>
   );
 }
