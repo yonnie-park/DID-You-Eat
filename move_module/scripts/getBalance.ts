ea2e1c8b9e740c9ba5fcaf1ee5cae12b79c23398
natreeum <projectkmscom@gmail.com>
2023-02-08 14:05:32 +0900

    add : move first commit

---
+++ move_module/scripts/getBalance.ts
@@ -0,0 +1,15 @@
+import { AptosClient, AptosAccount, CoinClient } from 'aptos';
+import { defAccount, network } from './values';
+
+const client = new AptosClient(network.NODE_URL);
+
+const coinClient = new CoinClient(client);
+
+const defaultAccount = new AptosAccount(
+  new Uint8Array(Buffer.from(defAccount.private_key)),
+  defAccount.maybeHexString
+);
+
+coinClient.checkBalance(defaultAccount).then((e) => {
+  console.log(e);
+});
---
+++ move_module/scripts/transaction.ts
@@ -0,0 +1,1 @@
+import { AptosClient } from 'aptos';
