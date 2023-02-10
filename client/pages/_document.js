8e91ce23623abefa5251b32bfa67ff864c569432
oreozzang <93peace@naver.com>
2023-02-10 14:51:53 +0900

    slash eliminate

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -3,9 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
-      </Head>
+      <Head></Head>
       <body id="body">
         <Main />
         <NextScript />
