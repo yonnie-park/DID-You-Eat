156ebaf1a42c701ac14e8e106cb6f28491d3fd84
oreozzang <93peace@naver.com>
2023-02-10 13:38:05 +0900

    redirect

--- client/pages/redirect.jsx
+++ client/pages/redirect.jsx
@@ -2,5 +2,10 @@ import GoogleRedirect from "@/src/components/GoogleRedirect";
 import React from "react";
 
 export default function Redirect() {
-  return <GoogleRedirect></GoogleRedirect>;
+  return (
+    <>
+      <GoogleRedirect></GoogleRedirect>
+      <h1>리다이렉트중</h1>
+    </>
+  );
 }
