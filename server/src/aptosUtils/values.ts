6862455d0fc786404a2c6fe8bdb235117cb16ad2
natreeum <projectkmscom@gmail.com>
2023-02-10 15:33:36 +0900

    asdf

--- server/src/aptosUtils/values.ts
+++ server/src/aptosUtils/values.ts
@@ -1,10 +1,10 @@
 export const network = {
-  NODE_URL: "https://fullnode.testnet.aptoslabs.com",
-  FAUCET_URL: "https://faucet.testnet.aptoslabs.com",
+  NODE_URL: 'https://fullnode.testnet.aptoslabs.com/v1',
+  FAUCET_URL: 'https://faucet.testnet.aptoslabs.com',
 };
 
 export const RESOURCE_ACCOUNT_ADDR =
-  "0x39352f60ff417edab7e766af81e8bf8fda21207cf4d9202772be8e6fe20b465d";
+  '0x39352f60ff417edab7e766af81e8bf8fda21207cf4d9202772be8e6fe20b465d';
 
-export const MODULE_NAME = "did_you_eat";
-export const MINT_TOKEN_FUNCTION_NAME = "mint_token";
+export const MODULE_NAME = 'did_you_eat';
+export const MINT_TOKEN_FUNCTION_NAME = 'mint_token';
